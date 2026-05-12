7be2d92cb17ee31e9e84f59a44a6a6ab0f9cdd31
Ozkar <ozcodx@gmail.com>
2026-05-12 09:15:50 -0500

    Fix item reference in grinding wheel recipe and update texture files

--- crafts/grinding_wheel.lua
+++ crafts/grinding_wheel.lua
@@ -1,7 +1,7 @@
 crafting.register_recipe({
     type = "grinding_stone",
     output = "exile_crafting_tables:grinding_wheel",
-    items = { "group:log 1", "group:granite_boulder 1", "tech:stick 6", "nodes_nature:sand 1" },
+    items = { "group:log 1", "nodes_nature:granite_boulder 1", "tech:stick 6", "nodes_nature:sand 1" },
     level = 1,
     always_known = true,
 })
