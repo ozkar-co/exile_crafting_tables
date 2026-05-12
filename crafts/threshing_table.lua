168965d97a77ab9cad6dbbb3352726a2807d5a1e
Ozkar <ozcodx@gmail.com>
2026-05-12 09:08:47 -0500

    Remove Threshing Table node and recipe in preparation for rework

--- crafts/threshing_table.lua
+++
@@ -1,7 +0,0 @@
-crafting.register_recipe({
-    type = "hammering_spot",
-    output = "exile_crafting_tables:threshing_sledge",
-    items = { "group:gravel 2", "group:log", "group:fibrous_plant 8" },
-    level = 1,
-    always_known = true,
-})
